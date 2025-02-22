b151f8979f3c678bf391d20c3e72f45df0189976
Dave Biersach <dbiersach@gmail.com>
2025-02-21 22:08:56 -0500

    Initial commit

---
+++ labs/Session 18 - Differential Equations/rc_circuit.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+"""rc_circuit.py"""
+
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.collections import LineCollection
+from matplotlib.ticker import (
+    AutoMinorLocator,
+    FormatStrFormatter,
+    MaxNLocator,
+    MultipleLocator,
+)
+
+# Read samples
+file_name = "rc_samples.csv"
+file_path = Path(__file__).parent / file_name
+times, volts = np.genfromtxt(file_path, delimiter=",", unpack=True)
+
+# Set times to be elapsed time & scale to seconds
+times -= times[0]
+times *= 1e-9
+
+# Find the middle time value
+mid_time = times[-1] / 2
+
+# Scale volts to fall between 0 and 3.3V
+volts /= 65535
+volts *= 3.3
+
+# Calculate theoretical performance curve
+V_s = 3.3  # Volts
+R = 10_121  # Ohms
+C = 0.00001069  # Farads
+tau = R * C
+t = np.linspace(0, mid_time, 100)
+v1_c = V_s * (1 - np.exp(-t / tau))  # Charge
+v2_c = V_s * np.exp(-t / tau)  # Decay
+
+# Create a plot window
+plt.figure(Path(__file__).name)
+plt.gca().set_facecolor("black")
+
+# Plot theoretical voltage
+plt.plot(t, v1_c, color="cyan", linewidth=2, label="Theory")
+plt.plot(t + mid_time, v2_c, color="cyan", linewidth=2)
+
+# Plot actual voltage
+# plt.plot(times, volts, color="magenta", linewidth=2, label="Actual")
+
+# Give the graph a title, axis labels, and display the legend
+plt.title("Capacitor Voltage vs. Time")
+plt.xlabel("Time (s)")
+plt.ylabel("Voltage (V)")
+plt.legend()
+
+# Set tick marks
+plt.gca().xaxis.set_major_locator(MaxNLocator(11))
+plt.gca().xaxis.set_major_formatter(FormatStrFormatter("%.2f"))
+plt.gca().xaxis.set_minor_locator(AutoMinorLocator(2))
+plt.gca().yaxis.set_major_locator(MultipleLocator(0.25))
+plt.grid(which="both", color="gray", linestyle="dotted", alpha=0.5)
+
+# Create straight lines to depict charging and discharging periods
+line_on = [(0, 0), (0, 3.3)]
+line_charging = [(0, 3.3), (mid_time, 3.3)]
+line_off = [(mid_time, 3.3), (mid_time, 0)]
+line_discharging = [(mid_time, 0), (times[-1], 0)]
+lc = LineCollection(
+    [line_on, line_charging, line_off, line_discharging],
+    color="yellow",
+    linewidth=2,
+    zorder=2.5,
+)
+plt.gca().add_collection(lc)
+
+# Draw dashed lines starting at offset 0, with 5 units "on" and 6 units "off"
+plt.axvline(0.1, color="yellow", linestyle=(0, (5, 6)), alpha=0.65)
+plt.axvline(mid_time + 0.1, color="yellow", linestyle=(0, (5, 6)), alpha=0.65)
+
+plt.show()
---
+++ labs/Session 18 - Differential Equations/scipy_pendulum.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""scipy_pendulum.py"""
+
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import solve_ivp
+
+
+def model(time, state_vector, phase_constant):
+    # Unpack current state vector (dependent variables)
+    omega, theta = state_vector
+    # Express differential equations
+    d_omega = -phase_constant * np.sin(theta)
+    d_theta = omega
+    return d_omega, d_theta
+
+
+def main():
+    # Precalculate phase constant
+    pendulum_length = 1.0  # meters
+    phase_constant = 9.81 / pendulum_length
+
+    # Set initial conditions
+    omega_initial = 0
+    theta_initial = np.deg2rad(45)
+
+    # Set model duration (seconds)
+    time_initial = 0
+    time_final = 10
+
+    # Numerically estimate the ODE using RK45 Method
+    sol = solve_ivp(
+        model,
+        (time_initial, time_final),  # tuple of time span
+        [omega_initial, theta_initial],  # initial state vector
+        max_step=0.01,
+        args=(phase_constant,),  # tuple of constants used in ODE
+    )
+    # Retrieve results of the solution
+    time_steps = sol.t
+    omega, theta = sol.y  # Unpack order like initial state vector
+
+    plt.figure(Path(__file__).name)
+    (plot1,) = plt.plot(time_steps, theta, lw=2, label=r"$\theta$ (rads)")
+    (plot2,) = plt.plot(time_steps, omega, lw=2, label=r"$\omega$ (rads/sec)")
+    plt.title("Simple Pendulum (RKF45 Method)")
+    plt.xlabel("Time (sec)")
+    plt.ylabel(r"Angular Displacement $\theta$ (rad)")
+    plt.twinx()
+    plt.ylabel(r"Angular Velocity $\omega$ (rad/s)")
+    plt.legend(
+        [plot1, plot2], [r"$\theta$", r"$\omega$"], framealpha=1.0, facecolor="white"
+    )
+    plt.grid("On")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
