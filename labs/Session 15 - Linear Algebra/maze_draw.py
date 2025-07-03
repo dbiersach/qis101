5869cb90b5c4c2189ab988ac34aa648f858cd5a1
Dave Biersach <dbiersach@gmail.com>
2025-07-02 22:04:37 -0400

    Fix maze validation

--- labs/Session 15 - Linear Algebra/maze_draw.py
+++ labs/Session 15 - Linear Algebra/maze_draw.py
@@ -13,7 +13,7 @@ from matplotlib.patches import Rectangle
 def validate_maze(maze):
     for y in range(10):
         for x in range(10):
-            cell = maze[y, x]
+            cell = int(maze[y, x])
             # Check CSV has a value in every cell
             if cell == -1:
                 print(f"Cell {y, x} has no value")
@@ -33,52 +33,52 @@ def validate_maze(maze):
                 return False
             # Check every cell agrees with its NORTH cell
             if y > 0:
-                cell2: int = maze[y - 1, x]
+                cell2: int = int(maze[y - 1, x])
                 if (cell & 1 == 1 and cell2 & 4 != 4) or (
                     cell & 1 != 1 and cell2 & 4 == 4
                 ):
                     print(
                         (
-                            f"Cell {y, x} and cell {y - 1, x}"
+                            f"Cell {y, x}={cell} and cell {y - 1, x}={cell2}"
                             " do not agree between NORTH/SOUTH"
                         )
                     )
                     return False
             # Check every cell agrees with its SOUTH cell
             if y < 9:
-                cell2 = maze[y + 1, x]
+                cell2 = int(maze[y + 1, x])
                 if (cell & 4 == 4 and cell2 & 1 != 1) or (
                     cell & 4 != 4 and cell2 & 1 == 1
                 ):
                     print(
                         (
-                            f"Cell {y, x} and cell {y + 1, x}"
+                            f"Cell {y, x}={cell} and cell {y + 1, x}={cell2}"
                             " do not agree between NORTH/SOUTH"
                         )
                     )
                     return False
             # Check every cell agrees with its EAST cell
             if x < 9:
-                cell2 = maze[y, x + 1]
+                cell2 = int(maze[y, x + 1])
                 if (cell & 2 == 2 and cell2 & 8 != 8) or (
                     cell & 2 != 2 and cell2 & 8 == 8
                 ):
                     print(
                         (
-                            f"Cell {y, x} and cell {y, x + 1}"
+                            f"Cell {y, x}={cell} and cell {y, x + 1}={cell2}"
                             " do not agree between EAST/WEST"
                         )
                     )
                     return False
             # Check every cell agrees with its WEST cell
             if x > 0:
-                cell2 = maze[y, x - 1]
+                cell2 = int(maze[y, x - 1])
                 if (cell & 8 == 8 and cell2 & 2 != 2) or (
                     cell & 8 != 8 and cell2 & 2 == 2
                 ):
                     print(
                         (
-                            f"Cell {y, x} and cell {y, x - 1}"
+                            f"Cell {y, x}={cell} and cell {y, x - 1}={cell2}"
                             " do not agree between EAST/WEST"
                         )
                     )
@@ -143,10 +143,9 @@ def main(file_name):
         file_path = file_path.with_suffix(".pickle")
         with open(file_path, "wb") as outfile:
             pickle.dump(m, outfile, pickle.HIGHEST_PROTOCOL)
-
-    plt.figure(f"{Path(__file__).name} ({file_name})")
-    plot_maze(plt.gca(), m)
-    plt.show()
+        plt.figure(f"{Path(__file__).name} ({file_name})")
+        plot_maze(plt.gca(), m)
+        plt.show()
 
 
 if __name__ == "__main__":
