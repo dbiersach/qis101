Fix APRE formula

labs/Session 10 - Numerical Analysis/circle_area.py
@@ -6,10 +6,12 @@ import scipy.integrate
 
 
 def f(x):
+    # This is the function we are numerically integrating
     return 4 * np.sqrt(1 - x**2)
 
 
 def F(x):
+    # This is the exact analytic integral of our function
     return 2 * (x * np.sqrt(1 - x**2) + np.arcsin(x))
 
 
@@ -27,32 +29,35 @@ def simpsons_rule(func, a, b, intervals):
     return dx / 3 * area
 
 
+def print_apre(observed, expected):
+    print(
+        "Absolute % Relative Error :"  # noqa
+        # Note: The % formatter automatically multiples value by 100
+        f"{abs((observed - expected) / expected):.14%}\n"
+    )
+
+
 def main():
     a, b, intervals = 0.0, 1.0, int(1e6)
 
-    print("\nIntegrating 4 * sqrt(1 - x^2)", end=" ")
-    print(f"over [{a}, {b}] using {intervals:,} intervals")
+    print("\nIntegrating f(x) = 4 * sqrt(1 - x^2)")
+    print(f" from {a} to {b}")
+    print(f" using {intervals:,} intervals\n")
 
     area_analytic = F(b) - F(a)
     print(f"Exact Analytic : {area_analytic:.14f}\n")
 
     area_lh = left_hand_rule(f, a, b, intervals)
     print(f"Left-hand Rule : {area_lh:.14f}")
-    print(
-        f"Absolute % Relative Error : {abs(area_lh - area_analytic) / abs(area_analytic):.14%}\n"
-    )
+    print_apre(area_lh, area_analytic)
 
     area_simp = simpsons_rule(f, a, b, intervals)
     print(f"Simpson's Rule : {area_simp:.14f}")
-    print(
-        f"Absolute % Relative Error : {abs(area_simp - area_analytic) / abs(area_analytic):.14%}\n"
-    )
+    print_apre(area_simp, area_analytic)
 
     area_scipy = scipy.integrate.quad(f, a, b)[0]
-    print(f"SciPy's quad() : {area_scipy:.14f}")
-    print(
-        f"Absolute % Relative Error : {abs(area_scipy - area_analytic) / abs(area_analytic):.14%}\n"
-    )
+    print(f"SciPy Quadrature : {area_scipy:.14f}")
+    print_apre(area_scipy, area_analytic)
 
 
 if __name__ == "__main__":