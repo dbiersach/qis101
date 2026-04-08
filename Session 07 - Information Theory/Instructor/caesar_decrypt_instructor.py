d9041e1edf786c0e572d447c025ce68b333d8a56
Dave Biersach <dbiersach@gmail.com>
2026-04-08 18:57:29 -0400

    Add more cell comments

--- Session 07 - Information Theory/Instructor/caesar_decrypt_instructor.py
+++ Session 07 - Information Theory/Instructor/caesar_decrypt_instructor.py
@@ -3,16 +3,16 @@
 
 from pathlib import Path
 
-import numpy as np
+FILE_NAME = "ciphertext1.txt"
+KEY_SHIFT = -45
 
-file_name = "ciphertext1.txt"
-key_shift = -45
-
-file_path = Path(__file__).parent / file_name
+# Read the file as bytes
+file_path = Path(__file__).parent / FILE_NAME
 with open(file_path, "rb") as f_in:
     f_bytes = bytearray(f_in.read())
 
-for i in np.arange(len(f_bytes)):
-    f_bytes[i] = (f_bytes[i] + key_shift) % 256
+# Apply the Caesar cipher shift
+f_bytes = bytearray((b + KEY_SHIFT) % 256 for b in f_bytes)
 
+# Decode file bytes to UTF-8 and display the result
 print(f_bytes.decode("utf-8", "ignore"))
