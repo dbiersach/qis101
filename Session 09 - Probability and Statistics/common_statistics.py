1262c74baee43abbd8b24e9bf0d37713f973e157
Dave Biersach <dbiersach@gmail.com>
2026-06-17 09:18:07 -0400

    Clean up comments

--- Session 09 - Probability and Statistics/common_statistics.py
+++ Session 09 - Probability and Statistics/common_statistics.py
@@ -4,17 +4,17 @@
 import collections
 import random
 
-# These functions are for demonstration purposes only.
-# In practice, you should use the built-in functions in the statistics module.
+# These functions are for demonstration purposes only
+# In practice, you should use the built-in functions in the statistics module
+# import statistics
+# statistics.mean(), statistics.median(), statistics.mode(), statistics.multimode()
 
 
 def mean(s):
-    # See statistics.mean()
     return sum(s) / len(s)
 
 
 def median(s):
-    # See statistics.median()
     t = sorted(s)
     i = len(t) // 2
     if len(t) % 2 == 1:
@@ -24,7 +24,6 @@ def median(s):
 
 
 def mode(s):
-    # See statistics.mode() and statistics.multimode()
     c = collections.Counter(s)
     max_c = max(c.values())
     if max_c == 1:
